a063a5729e75eb3e241fb4c94a964ac72ed10931
zyc <zyclyy@sina.cn>
2014-07-11 17:25:36 +0800

    7-11

---
+++ shell/match.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#set -e
+
+#read -p "please input the IP:" IP
+
+#echo $IP | awk -v ip=$IP '{host[$1]=$2} END{print host[ip]}' /home/zyc/Desktop/hosts
+
+#egrep "$IP" ~/Desktop/hosts | awk '{print $2}'
+
+#awk 'BEGIN{var="'$1'"} {if(var==$1)print $2}' ~/Desktop/hosts
+
+#awk '{if($1 ~/'$1'/)print $2}' ~/Desktop/hosts
+
+
+
